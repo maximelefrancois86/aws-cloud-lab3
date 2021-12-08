9705aea7a21eb30fceb35ec883f21d224a00e7e4
Mariano1Schloss <mathieu.delbos34@gmail.com>
2021-12-08 11:47:56 +0100

    created SQS queues inbox & outbox

--- src/main/java/emse/EC2Worker.java
+++ src/main/java/emse/EC2Worker.java
@@ -1,7 +1,42 @@
 package emse;
 
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.CreateQueueRequest;
+import software.amazon.awssdk.services.sqs.model.GetQueueUrlRequest;
+import software.amazon.awssdk.services.sqs.model.GetQueueUrlResponse;
+import software.amazon.awssdk.services.sqs.model.SqsException;
+
 public class EC2Worker {
 
+    public static String createQueue(SqsClient sqsClient, String queueName ) {
 
+        try {
+            System.out.println("\nCreate Queue");
 
+            CreateQueueRequest createQueueRequest = CreateQueueRequest.builder()
+                    .queueName(queueName)
+                    .build();
+
+            sqsClient.createQueue(createQueueRequest);
+
+            System.out.println("\nGet queue url");
+
+            GetQueueUrlResponse getQueueUrlResponse =
+                    sqsClient.getQueueUrl(GetQueueUrlRequest.builder().queueName(queueName).build());
+            String queueUrl = getQueueUrlResponse.queueUrl();
+            return queueUrl;
+
+        } catch (SqsException e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+        return "";
+    }
+
+    public static void main (String args []){
+        SqsClient sqsClient = SqsClient.builder()
+                .build();
+        String inbox=createQueue(sqsClient, "INBOX");
+        String outbox=createQueue(sqsClient, "OUTBOX");
+    }
 }
