098d780989a5bce252fbe7210fb376a5bb2244d7
Mariano1Schloss <mathieu.delbos34@gmail.com>
2021-12-08 12:02:10 +0100

    added receive minutes in EC2Worker , timer in main to receive messages every minutes

--- src/main/java/emse/EC2Worker.java
+++ src/main/java/emse/EC2Worker.java
@@ -1,10 +1,9 @@
 package emse;
 
 import software.amazon.awssdk.services.sqs.SqsClient;
-import software.amazon.awssdk.services.sqs.model.CreateQueueRequest;
-import software.amazon.awssdk.services.sqs.model.GetQueueUrlRequest;
-import software.amazon.awssdk.services.sqs.model.GetQueueUrlResponse;
-import software.amazon.awssdk.services.sqs.model.SqsException;
+import software.amazon.awssdk.services.sqs.model.*;
+
+import java.util.List;
 
 public class EC2Worker {
 
@@ -33,10 +32,39 @@ public class EC2Worker {
         return "";
     }
 
+    public static List<Message> receiveMessages(SqsClient sqsClient, String queueUrl) {
+
+        System.out.println("\nReceive messages");
+
+        try {
+            ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder()
+                    .queueUrl(queueUrl)
+                    .maxNumberOfMessages(5)
+                    .build();
+            List<Message> messages = sqsClient.receiveMessage(receiveMessageRequest).messages();
+            return messages;
+        } catch (SqsException e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+        return null;
+    }
+
     public static void main (String args []){
         SqsClient sqsClient = SqsClient.builder()
                 .build();
         String inbox=createQueue(sqsClient, "INBOX");
         String outbox=createQueue(sqsClient, "OUTBOX");
+
+        Long lastRecordedTime=System.currentTimeMillis();
+        List<Message> messages=null;
+        while(true){
+            Long timer=System.currentTimeMillis();
+            //we receive messages every minute
+            if (timer-lastRecordedTime>60000){
+                messages.addAll(receiveMessages(sqsClient,inbox));
+            }
+
+        }
     }
 }
