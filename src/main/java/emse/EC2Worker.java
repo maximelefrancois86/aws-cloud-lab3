19636a4d71b2c90c4bad086dbcfed973714496a4
Mariano1Schloss <mathieu.delbos34@gmail.com>
2022-01-03 17:59:01 +0100

    csv parser

--- src/main/java/emse/EC2Worker.java
+++ src/main/java/emse/EC2Worker.java
@@ -6,24 +6,14 @@ import software.amazon.awssdk.services.sqs.model.*;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
-/*TO DO:
-* Supprimer les fichiers ec2 csv et data txt en fin de boucle
-* Adapter le temps d'attente
-* Optimer les messages affichés en console (ex receive messages)
-* Chercher la question 2
- */
-
-
-import static java.lang.Thread.sleep;
 
 public class EC2Worker {
 
-    public static String createQueue(SqsClient sqsClient, String queueName ) {
+    public static String createQueue(SqsClient sqsClient, String queueName) {
 
         try {
-            System.out.println("\nCreate Queue");
+            System.out.println("\nCreating Queue");
 
             CreateQueueRequest createQueueRequest = CreateQueueRequest.builder()
                     .queueName(queueName)
@@ -31,7 +21,7 @@ public class EC2Worker {
 
             sqsClient.createQueue(createQueueRequest);
 
-            System.out.println("\nGet queue url");
+            System.out.println("\nGetting queue url");
 
             GetQueueUrlResponse getQueueUrlResponse =
                     sqsClient.getQueueUrl(GetQueueUrlRequest.builder().queueName(queueName).build());
@@ -47,7 +37,7 @@ public class EC2Worker {
 
     public static List<Message> receiveMessages(SqsClient sqsClient, String queueUrl) {
 
-        System.out.println("\nReceive messages");
+        System.out.println("Checking for messages ...");
 
         try {
             ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder()
@@ -85,36 +75,38 @@ public class EC2Worker {
         }
     }
 
-    public static void main (String args []){
+    public static void main(String args[]) {
         SqsClient sqsClient = SqsClient.builder()
                 .build();
-        String inbox=createQueue(sqsClient, "INBOX");
-        String outbox=createQueue(sqsClient, "OUTBOX");
-
-        Long lastRecordedTime=System.currentTimeMillis();
-
-        while(true){
-            Long timer=System.currentTimeMillis();
-            //we receive messages every minute
-            if (timer-lastRecordedTime>10000){
-                List<Message> messages=receiveMessages(sqsClient,inbox);
+        String inbox = createQueue(sqsClient, "INBOX");
+        System.out.println("INBOX queue created");
+        String outbox = createQueue(sqsClient, "OUTBOX");
+        System.out.println("OUTBOX queue created");
 
 
+        Long lastRecordedTime = System.currentTimeMillis();
 
-                lastRecordedTime=timer;
-                if ( messages.size()==2){
+        while (true) {
+            Long timer = System.currentTimeMillis();
+            //we cjeck for messages every 10 seconds
+            if (timer - lastRecordedTime > 10000) {
+                List<Message> messages = receiveMessages(sqsClient, inbox);
+
+
+                lastRecordedTime = timer;
+                if (messages.size() == 2) {
                     System.out.println("a message has been receved");
                     try {
                         //extracting the bucket name
-                        System.out.println("number of messages :"+messages.size());
-                        String bucket=messages.get(0).body();
-                        System.out.println("Bucket name : "+bucket);
+                        System.out.println("number of messages :" + messages.size());
+                        String bucket = messages.get(0).body();
+                        System.out.println("Bucket name : " + bucket);
 
                         //extracting the file name
-                        String fileName=messages.get(1).body();
-                        System.out.println("File name : "+fileName);
+                        String fileName = messages.get(1).body();
+                        System.out.println("File name : " + fileName);
                         //Getting the cvs file
-                        S3ControllerGetObject.main(new String[]{bucket,fileName,"ec2sales.csv"});
+                        S3ControllerGetObject.main(new String[]{bucket, fileName, "ec2sales.csv"});
                         //Anaylizing the csv file
                         //S3ControllerAnalyseData.main(new String[]{"ec2sales.csv"});
                         try {
@@ -124,12 +116,12 @@ public class EC2Worker {
                         } catch (CsvException e) {
                             e.printStackTrace();
                         }
-                        deleteMessages(sqsClient,inbox,messages);
+                        deleteMessages(sqsClient, inbox, messages);
                         System.out.println("\n" + "Writing the file into a bucket in the Amazon S3");
                         S3ControllerPutObject.main(new String[]{bucket, "data.txt", "data.txt"});
                         System.out.println("\n" + "Sending a message to the Inbox queue with the bucket and file names");
-                        String queueURl ="https://sqs.us-west-2.amazonaws.com/528939267914/";
-                        SQSSendMessage.sendMessages(sqsClient, queueURl+"OUTBOX", bucket, "data.txt");
+                        String queueURl = "https://sqs.us-west-2.amazonaws.com/528939267914/";
+                        SQSSendMessage.sendMessages(sqsClient, queueURl + "OUTBOX", bucket, "data.txt");
 
                     } catch (SqsException e) {
                         System.err.println(e.awsErrorDetails().errorMessage());
